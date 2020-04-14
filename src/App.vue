882b3e9adf09a1e6c9c058daab54af27eea55eec
elvinos <aep.charles@gmail.com>
2020-04-14 14:32:48 +0100

    Intial Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <h1>Hello this is a test</h1>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "assets/sass/main";
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ this.message }}</h1>
+    <input type="text" v-model="inputValue" />
+    <button @click="onClick">Send</button>
+    <div>
+      {{ response }}
+    </div>
+  </div>
+</template>
+
+<script>
+  // export const eel = window.eel;
+  eel.set_host( 'ws://localhost:8080' );
+
+    // Expose the `sayHelloJS` function to Python as `say_hello_js`
+  function sayHelloJS(x) {
+    console.log( 'Hello from ' + x )
+  }
+  // WARN: must use window.eel to keep parse-able eel.expose{...}
+  eel.expose( sayHelloJS, 'say_hello_js' );
+
+  // Test calling sayHelloJS, then call the corresponding Python function
+  sayHelloJS( 'Javascript World!' );
+  eel.say_hello_py( 'Javascript World!' );
+
+
+  export default {
+    name: 'HelloWorld',
+    data: function () {
+      return {
+        message: "",
+        inputValue: "",
+        response: ""
+      }
+    },
+    mounted: function () {
+      eel.hello_world()((val) => {
+        // Receiving a value from Python
+        this.message = val
+      })
+    },
+    methods: {
+      onClick() {
+        // Passing values to Python
+        eel.print_string(this.inputValue)((val) => {
+          // Return response from Python
+          this.response = val
+        })
+      }
+    }
+  }
+</script>
